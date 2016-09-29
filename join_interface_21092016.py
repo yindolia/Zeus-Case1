dde170fe43f2a875624b97e1f12f05720702ee0b
yindolia <yindolia@gmail.com>
2016-09-29 14:21:26 +0530

    Update join_interface_21092016.py

--- join_interface_21092016.py
+++ join_interface_21092016.py
@@ -7,7 +7,10 @@ Created on Wed Sep 28 16:21:01 2016
 
 from itertools import groupby
 
+t = open('C:/Users/ysingh2/Desktop/techs/t-interfac', 'w+')
 with open('C:/Users/ysingh2/Desktop/techs/sample-interface.txt') as f:
     for key, group in groupby(f, lambda s: s.startswith('Ethernet1')):
-        Full= '\N'.join(s.rstrip('\n') for s in group)
-        print Full
+        joinedLog= '\N'.join(s.rstrip('\n') for s in group)
+        t.write(joinedLog)
+        t.write('\n')
+        print joinedLog
